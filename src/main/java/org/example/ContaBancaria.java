4c91f80a0c1dcd33e8f6fe267d5523e3d6a704ed
Riki Ventura <riki.ventura19@gmail.com>
2026-04-27 22:06:28 -0300

    feat: adicionar pagamento de boleto

--- src/main/java/org/example/ContaBancaria.java
+++ src/main/java/org/example/ContaBancaria.java
@@ -1,4 +1,79 @@
 package org.example;
 
 public class ContaBancaria {
+
+    /*saldo: Dinheiro na conta;
+      limiteChequeEspecial: Quanto pode usar além do saldo;
+      usadoChequeEspecial: quanto já foi usado
+       */
+    private double saldo;
+    private double limiteChequeEspecial;
+    private double usadoChequeEspecial;
+
+    public double getSaldo(){
+        return saldo;
+    }
+    public double getLimiteChequeEspecial(){
+        return limiteChequeEspecial;
+    }
+    public double getUsadoChequeEspecial(){
+        return usadoChequeEspecial;
+    }
+
+    // Os métodos get permitem acessar os valores dos atributos sem deixar os atributos públicos.
+
+    public ContaBancaria (double valorInicial){
+        //definir saldo
+        saldo = valorInicial;
+        // se valor inicial for menor que R$500,00 cheque especial terá R$50,00 de limite.
+        if (saldo<=500){
+            limiteChequeEspecial=50;
+        }
+        //se valor inicial for maior que R$500,00 cheque especial terá o valor de 50% do valor inicial.
+        else {
+            limiteChequeEspecial = saldo * 0.5;
+        }
+        //valor usado do cheque especial
+        usadoChequeEspecial =0;
+    }
+public void sacar(double valor){
+        //valor a ser sacado é o Saldo + LimeteChequeEspecial.
+    if (saldo>= valor) {
+        //tira do valor o saldo
+        saldo = saldo - valor;
+    }else {
+        //tira o valor do saldo + limiteChequeEspecial.
+        double falta = valor - saldo;
+        double disponivelCheque = limiteChequeEspecial - usadoChequeEspecial;
+
+        if (falta <= disponivelCheque){
+            saldo = 0;
+            usadoChequeEspecial += falta;
+        }else{
+            System.out.println("Saldo insuficiente");
+        }
+    }
+
+}
+public void depositar(double deposito) {
+    //valor a ser depositado será usado para pagar a divida caso o cheque especial usado seja maior que 0.
+    if (usadoChequeEspecial > 0) {
+        //Está dizendo, meu cheque especial usado é maior que 0 (está perguntando se usei um valor maior que 0 do meu cheque especial, se sim vamos pegar parte do valor depositado mais 20% se sobrar para pagar a divida.)
+        double taxa = usadoChequeEspecial * 0.2;
+        double totalDivida = usadoChequeEspecial + taxa;
+
+        if (deposito >= totalDivida) {
+            deposito -= totalDivida;
+            usadoChequeEspecial = 0;
+            saldo += deposito;
+        } else {
+            usadoChequeEspecial = totalDivida - deposito;
+        }
+    }else{
+        saldo += deposito;
+    }
+}
+    public void pagarBoleto(double valor) {
+        sacar(valor);
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,2 +1,65 @@
 package org.example;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor inicial da conta: ");
+        double valorInicial = scanner.nextDouble();
+
+        ContaBancaria conta = new ContaBancaria(valorInicial);
+
+        int opcao = 0;
+
+        // O Loop while é executado até que alguma condição seja atendida.
+
+        while (opcao != 7){
+            System.out.println("\n========  MENU BANCO  =======");
+            System.out.println("1 - Consultar Saldo");
+            System.out.println("2 - Consultar Cheque Especial");
+            System.out.println("3 - Depositar");
+            System.out.println("4 - Sacar");
+            System.out.println("5 - Verificar uso do Cheque Especial");
+            System.out.println("6 - Pagar Boleto");
+            System.out.println("7 - Sair");
+            System.out.println("Escolha uma opção:");
+
+            opcao = scanner.nextInt();
+
+            if (opcao == 1){
+                System.out.println("Saldo: R$ " + conta.getSaldo());
+            } else if (opcao == 2){
+                System.out.println("Limite Cheque Especial: R$ " + conta.getLimiteChequeEspecial());
+                System.out.println("Cheque Especial usado R$: " + conta.getUsadoChequeEspecial());
+            } else if (opcao == 3) {
+                System.out.println("Digite o Valor do Deposito");
+                double deposito = scanner.nextDouble();
+                conta.depositar(deposito);
+                System.out.println("Depósito realizado com sucesso!");
+            } else if (opcao == 4) {
+                System.out.println("Digite o valor do saque:");
+                double valorSaque = scanner.nextDouble();
+                conta.sacar(valorSaque);
+            } else if (opcao == 5) {
+                if (conta.getUsadoChequeEspecial() > 0){
+                    System.out.println("A conta está usando Cheque Especial.");
+                }else {
+                    System.out.println("A conta não está usando Cheque Especial.");
+                }
+            } else if (opcao == 6) {
+                System.out.println("Digite o valor do Boleto: ");
+                double valorBoleto = scanner.nextDouble();
+                conta.pagarBoleto(valorBoleto);
+                System.out.println("Boleto pago com sucesso!");
+            } else if (opcao == 7) {
+                System.out.println("Sistema encerrado.");
+            } else {
+                System.out.println("Opção inválida");
+            }
+        }
+        scanner.close();
+    }
+}
